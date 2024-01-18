e09cf649a800ed55bbb99d7dcb36618e61cab734
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-01-18 08:45:16 -0300

    Update Cabecalho.jsx

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,9 +1,19 @@
+import Link from "next/link.js";
 import Menu from "./Menu.jsx";
+import Image from "next/image";
 
 export default function Cabecalho() {
   return (
     <header>
-      <h1>PetShop</h1>
+      <h1>
+        <Image
+          src="/images/logo.png"
+          width={48}
+          height={48}
+          alt="Patinha dentro de um coração"
+        />
+        <Link href="/">PetShop</Link>
+      </h1>
       <Menu />
     </header>
   );
