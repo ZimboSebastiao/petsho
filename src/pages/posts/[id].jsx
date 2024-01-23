eafc7e85e138dd915a7a03d646ac59e6c27cbf57
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-01-23 11:24:14 -0300

    adição da getStaticPaths

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -2,6 +2,18 @@ import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
 
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  console.log(id);
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
 export default function post() {
   return (
     <>
@@ -22,4 +34,8 @@ export default function post() {
   );
 }
 
-const StyledPost = styled.article``;
+const StyledPost = styled.article`
+  h2::before {
+    content: "📑 ";
+  }
+`;
